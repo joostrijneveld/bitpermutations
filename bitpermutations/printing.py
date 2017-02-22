bc9388e87727c3a89cf2bd5f804e97db83baadbc
Joost Rijneveld <joost@joostrijneveld.nl>
2017-02-22 11:18:45 +0100

    Move regfunc to memfunc conversion to utilities

--- bitpermutations/printing.py
+++ bitpermutations/printing.py
@@ -1,23 +1,7 @@
-from .data import Register, MemoryFragment
+from .data import MemoryFragment
 import bitpermutations.instructions as instructions
 import bitpermutations.data as data
-from .instructions import vmovdqu
-from functools import wraps
-
-
-def reg_to_memfunc(f, in_size, out_size, per_reg=256):
-    """ Makes a function that operates on registers use memory instead. """
-    @wraps(f)
-    def memfunc(out_data, in_data):
-        src = [Register(per_reg) for _ in range(in_size)]
-        dst = [Register(per_reg) for _ in range(out_size)]
-
-        for reg, mem in zip(src, in_data):
-            vmovdqu(reg, mem)
-        f(dst, src)
-        for mem, reg in zip(out_data, dst):
-            vmovdqu(mem, reg)
-    return memfunc
+from .utils import reg_to_memfunc
 
 
 def print_memfunc(f, in_size, out_size, per_reg=256):
--- bitpermutations/utils.py
+++ bitpermutations/utils.py
@@ -1,3 +1,25 @@
+from functools import wraps
+
 
 def split_in_size_n(l, n):
     return [l[i:i + n] for i in range(0, len(l), n)]
+
+
+def reg_to_memfunc(f, in_size, out_size, per_reg=256):
+    """ Makes a function that operates on registers use memory instead. """
+
+    # To prevent circular imports
+    from .instructions import vmovdqu
+    from .data import Register
+
+    @wraps(f)
+    def memfunc(out_data, in_data):
+        src = [Register(per_reg) for _ in range(in_size)]
+        dst = [Register(per_reg) for _ in range(out_size)]
+
+        for reg, mem in zip(src, in_data):
+            vmovdqu(reg, mem)
+        f(dst, src)
+        for mem, reg in zip(out_data, dst):
+            vmovdqu(mem, reg)
+    return memfunc
