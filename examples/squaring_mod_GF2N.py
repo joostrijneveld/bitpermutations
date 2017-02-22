abedb00267d12a3c165b8455154bcab5e13f31fe
Joost Rijneveld <joost@joostrijneveld.nl>
2017-02-22 16:36:45 +0100

    Add implementation of squaring mod 701

--- examples/squaring_mod_GF2N.py
+++ examples/squaring_mod_GF2N.py
@@ -1,8 +1,8 @@
 from bitpermutations.data import (ONE, ZERO, Register,
                                   Mask, IndicesMask, MaskRegister)
 import bitpermutations.instructions as x86
-import bitpermutations.data as x86data
 from bitpermutations.printing import print_reg_to_memfunc
+
 
 def gen_sequence(e, N):
     def interleave(seq):
@@ -29,7 +29,6 @@ def sequence_to_registers(dst, seq):
             break
     else:
         if len(seq) > 0:
-            print(seq)
             raise Exception("Sequence did not fit in registers; "
                             "{} elements remaining".format(len(seq)))
 
@@ -43,9 +42,259 @@ def registers_to_sequence(registers):
     return result
 
 
-def square(dst, src):
-    """ Expects three 256-bit registers containing the polynomial """
-    pass
+def square_701(out_data, in_data):
+    """ Operates on MemoryFragments containing the polynomials """
+
+    r = Register()
+
+    r_out = [Register() for _ in range(3)]
+    r_out_b = [Register() for _ in range(3)]
+    r_out_c = [Register() for _ in range(3)]
+    result = [Register() for _ in range(3)]
+
+    t1 = Register()
+    t2 = Register()
+    shifted = t3 = Register()
+
+    for i in range(2, -1, -1):
+        x86.vmovdqu(r, in_data[i])
+        for j in range(0, 8):
+            x86.macro_v256rol(shifted, r, j, t1, t2)
+            if i == 0:
+                if j < 4:
+                    if j == 0:
+                        mask = '00000001' * 32
+                    if j == 1:
+                        mask = '00000100' * 32
+                    if j == 2:
+                        mask = '00010000' * 32
+                    if j == 3:
+                        mask = '01000000' * 32
+
+                    if j == 0:
+                        x86.vpand(r_out[i], shifted, Mask(mask))
+                    else:
+                        x86.vpand(t1, shifted, Mask(mask))
+                        x86.vpxor(r_out[i], r_out[i], t1)
+                elif 4 <= j < 8:
+                    if j == 4:
+                        mask = '00000001' * 32
+                    if j == 5:
+                        mask = '00000100' * 32
+                    if j == 6:
+                        mask = '00010000' * 32
+                    if j == 7:
+                        mask = '01000000' * 32
+
+                    if j == 4:
+                        x86.vpand(r_out_b[i], shifted, Mask(mask))
+                    else:
+                        x86.vpand(t1, shifted, Mask(mask))
+                        x86.vpxor(r_out_b[i], r_out_b[i], t1)
+            if i == 1:
+                if j < 4:
+                    if j == 0:
+                        mask = '00100000' * 19 + '00100000' + '00000001' * 12
+                    if j == 1:
+                        mask = '10000000' * 19 + '10000000' + '00000100' * 12
+                    if j == 2:
+                        mask = '00000000' * 19 + '00000000' + '00010000' * 12
+                    if j == 3:
+                        mask = '00000000' * 19 + '00000000' + '01000000' * 12
+
+                    if j == 0:
+                        x86.vpand(r_out[i], shifted, Mask(mask))
+                    else:
+                        x86.vpand(t1, shifted, Mask(mask))
+                        x86.vpxor(r_out[i], r_out[i], t1)
+                if 2 <= j < 6:
+                    if j == 2:
+                        mask = '00000010' * 19 + '00000010' + '00000000' * 11 + '00000010'  # noqa: E501
+                    if j == 3:
+                        mask = '00001000' * 19 + '00001000' + '00000000' * 12
+                    if j == 4:
+                        mask = '00100000' * 19 + '00100000' + '00000000' * 12
+                    if j == 5:
+                        mask = '10000000' * 19 + '10000000' + '00000000' * 12
+
+                    if j == 2:
+                        x86.vpand(r_out_b[i], shifted, Mask(mask))
+                    else:
+                        x86.vpand(t1, shifted, Mask(mask))
+                        x86.vpxor(r_out_b[i], r_out_b[i], t1)
+                if 4 <= j < 8:
+                    if j == 4:
+                        mask = '00000000' * 19 + '00000001' + '00000001' * 11 + '00000000'  # noqa: E501
+                    if j == 5:
+                        mask = '00000000' * 19 + '00000100' + '00000100' * 11 + '00000000'  # noqa: E501
+                    if j == 6:
+                        mask = '00000010' * 19 + '00010000' + '00010000' * 11 + '00000010'  # noqa: E501
+                    if j == 7:
+                        mask = '00001000' * 19 + '00000000' + '01000000' * 11 + '00001000'  # noqa: E501
+
+                    if j == 4:
+                        x86.vpand(r_out_c[i], shifted, Mask(mask))
+                    else:
+                        x86.vpand(t1, shifted, Mask(mask))
+                        x86.vpxor(r_out_c[i], r_out_c[i], t1)
+
+            if i == 2:
+                if j < 2:
+                    if j == 0:
+                        mask = '00100000' * 32
+                    if j == 1:
+                        mask = '10000000' * 32
+
+                    if j == 0:
+                        x86.vpand(r_out[i], shifted, Mask(mask))
+                    else:
+                        x86.vpand(t1, shifted, Mask(mask))
+                        x86.vpxor(r_out[i], r_out[i], t1)
+                if 2 <= j < 6:
+                    if j == 2:
+                        mask = '00000010' * 32
+                    if j == 3:
+                        mask = '00001000' * 32
+                    if j == 4:
+                        mask = '00100000' * 32
+                    if j == 5:
+                        mask = '10000000' * 32
+
+                    if j == 2:
+                        x86.vpand(r_out_b[i], shifted, Mask(mask))
+                    else:
+                        x86.vpand(t1, shifted, Mask(mask))
+                        x86.vpxor(r_out_b[i], r_out_b[i], t1)
+                if 6 <= j < 8:
+                    if j == 6:
+                        mask = '00000010' * 32
+                    if j == 7:
+                        mask = '00001000' * 32
+
+                    if j == 6:
+                        x86.vpand(r_out_c[i], shifted, Mask(mask))
+                    else:
+                        x86.vpand(t1, shifted, Mask(mask))
+                        x86.vpxor(r_out_c[i], r_out_c[i], t1)
+
+    # move bit 511 to r_out_b[2]
+    x86.vpand(t1, r_out_b[1], Mask('0001'))
+    x86.vpxor(r_out_b[2], r_out_b[2], t1)
+
+    highbyte_mask = Mask(2 * ('1' + '0' * 15))
+    highbyte_nmask = Mask(2 * ('0' + '1' * 15))
+
+    mask = IndicesMask(2*sum(zip(range(8), [None] * 8), ()))
+    x86.vpshufb(result[0], r_out[0], mask)
+
+    mask = IndicesMask(2*sum(zip([None] * 8, range(1, 9)), ()))
+    x86.vpshufb(t1, r_out_b[0], mask)
+    x86.vpxor(result[0], t1, result[0])
+
+    x86.vextracti128(result[1], result[0], 1)
+    mask = IndicesMask(2*sum(zip(range(8, 16), [None] * 8), ()))
+    x86.vpshufb(result[2], r_out[0], mask)
+
+    mask = IndicesMask(2*(sum(zip([None] * 8, range(9, 16)), ()) + (None, 0)))
+    x86.vpshufb(t1, r_out_b[0], mask)
+    x86.vpand(t2, t1, highbyte_mask)
+    x86.vpand(t1, t1, highbyte_nmask)
+    x86.vpermq(t2, t2, '01001100')
+    x86.vpxor(t1, t1, t2)
+    x86.vpxor(result[2], t1, result[2])
+
+    x86.vinserti128(result[0], result[0], result[2], 1)
+    x86.vextracti128(t1, result[2], 1)
+    x86.vinserti128(result[1], result[1], t1, 1)
+
+    # ---
+
+    mask = IndicesMask(sum(zip(range(8), [None] * 8), ()) +
+                       sum(zip([None] * 8, range(8), ), ()))
+    x86.vpshufb(result[2], r_out[1], mask)
+
+    mask = IndicesMask(2*sum(zip([None] * 8, range(1, 9)), ()))
+    x86.vpshufb(t1, r_out_c[1], mask)
+    x86.vpxor(result[2], t1, result[2])
+
+    mask = IndicesMask((None,)*16+sum(zip(range(8), [None] * 8), ()))
+    x86.vpshufb(t1, r_out_b[1], mask)
+    x86.vpxor(result[2], t1, result[2])
+
+    x86.vextracti128(t2, result[2], 1)
+    x86.vpermq(t2, t2, '11010011')
+    x86.vpxor(result[0], t2, result[0])
+
+    mask = IndicesMask(sum(zip(range(8, 12), [None] * 8), ()) +
+                       sum(zip([None] * 8, range(12, 16)), ()) +
+                       sum(zip([None] * 8, range(8, 16), ), ()))
+    x86.vpshufb(t1, r_out[1], mask)
+
+    mask = IndicesMask(2*(sum(zip([None] * 7, range(9, 16)), ()) + (None, 0)))
+    x86.vpshufb(t2, r_out_c[1], mask)
+    x86.vpand(t3, t2, highbyte_mask)
+    x86.vpand(t2, t2, highbyte_nmask)
+    x86.vpermq(t3, t3, '01001100')
+    x86.vpxor(t2, t3, t2)
+    x86.vpxor(t1, t1, t2)
+
+    mask = IndicesMask(2*sum(zip(range(8, 16), [None] * 8), ()))
+    x86.vpshufb(t2, r_out_b[1], mask)
+    x86.vpxor(t2, t2, t1)
+    x86.vpand(t3, t2, Mask('0001'))
+    x86.vinserti128(result[2], result[2], t3, 1)
+    # complete first 0-350
+
+    x86.vpand(t1, t2, Mask('0110'))
+    x86.vpermq(t1, t1, '10000001')
+    x86.vpxor(result[0], result[0], t1)
+
+    x86.vpand(t1, t2, Mask('1000'))
+    x86.vpermq(t1, t1, '00000011')
+    x86.vpxor(result[1], t1, result[1])
+
+    mask = IndicesMask(2*sum(zip(range(8), [None] * 8), ()))
+    x86.vpshufb(t1, r_out_b[2], mask)
+
+    mask = IndicesMask(2*sum(zip([None] * 8, range(1, 9)), ()))
+    x86.vpshufb(t2, r_out_c[2], mask)
+    x86.vpxor(t1, t1, t2)
+
+    mask = IndicesMask(2*sum(zip([None] * 8, range(8)), ()))
+    x86.vpshufb(t2, r_out[2], mask)
+    x86.vpxor(t1, t1, t2)
+
+    x86.vinserti128(t3, t3, t1, 0)
+    x86.vpermq(t3, t3, '11010011')
+    x86.vpxor(result[1], t3, result[1])
+
+    x86.vextracti128(t3, t1, 1)
+    x86.vpermq(t3, t3, '11010011')
+    x86.vpxor(result[2], t3, result[2])
+
+    mask = IndicesMask(2*sum(zip(range(8, 16), [None] * 8), ()))
+    x86.vpshufb(t1, r_out_b[2], mask)
+
+    mask = IndicesMask(2*sum(zip([None] * 8, range(8, 16)), ()))
+    x86.vpshufb(t2, r_out[2], mask)
+    x86.vpxor(t1, t1, t2)
+
+    mask = IndicesMask(2*(sum(zip([None] * 7, range(9, 16)), ()) + (None, 0)))
+    x86.vpshufb(t2, r_out_c[2], mask)
+    x86.vpand(t3, t2, highbyte_mask)
+    x86.vpand(t2, t2, highbyte_nmask)
+    x86.vpermq(t3, t3, '01001100')
+    x86.vpxor(t2, t3, t2)
+    x86.vpxor(t1, t2, t1)
+
+    x86.vpermq(t2, t1, '00111111')
+    x86.vpxor(result[1], result[1], t2)
+
+    x86.vpermq(t3, t1, '11111101')
+    x86.vpxor(result[2], result[2], t3)
+
+    for i in range(3):
+        x86.vmovdqu(out_data[i], result[i])
 
 
 def square_350_701(dst, src):
--- examples/test_squaring_mod_GF2N.py
+++ examples/test_squaring_mod_GF2N.py
@@ -1,9 +1,20 @@
 import unittest
 import squaring_mod_GF2N as sqGF2N
-from bitpermutations.data import Register
+from bitpermutations.data import Register, MemoryFragment
 
 
 class TestSquaringModGF2N(unittest.TestCase):
+
+    def test_square_701(self):
+        src = [MemoryFragment(256, '{}(%rsi)'.format(i*32)) for i in range(3)]
+        dst = [MemoryFragment(256, '{}(%rdi)'.format(i*32)) for i in range(3)]
+        seq = sqGF2N.gen_sequence(1, 701)
+        sqGF2N.sequence_to_registers(src, range(0, 701))
+
+        sqGF2N.square_701(dst, src)
+        result = sqGF2N.registers_to_sequence(dst)
+
+        self.assertEqual(result, seq)
 
     def test_square_350_701(self):
         src = [Register(256) for _ in range(3)]
